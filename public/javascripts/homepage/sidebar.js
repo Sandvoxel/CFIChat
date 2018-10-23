834f5b1930460daf98c4e69a6314940f1ce8e039
Sandvoxel <kovalchickzz@gmail.com>
2018-10-22 21:37:23 -0500

    change nav animation to come from corner insted of side

--- public/javascripts/homepage/sidebar.js
+++ public/javascripts/homepage/sidebar.js
@@ -1,18 +1,22 @@
 function OpenCloseSidebar(){
+    document.getElementById("navbar-button").classList.toggle("active");
     document.getElementById("nav-menu-icon").classList.toggle("active");
 
 
-    SidebarUpdate(document.getElementById("nav-menu-icon").classList.contains("active"));
+    SidebarUpdate(document.getElementById("navbar-button").classList.contains("active"));
 }
 
 
 function SidebarUpdate(active){
     if(active){
-        //document.getElementById("nav-listing").style.height = "272px";
+        document.getElementById("nav-listing").style.height = "272px";
         document.getElementById("nav-listing").style.width = "201.609px";
+        //document.getElementById("nav-listing").style.lineHeight = "1.8em";
     }else{
-        //document.getElementById("nav-listing").style.height = "0px";
+        document.getElementById("nav-listing").style.height = "0px";
         document.getElementById("nav-listing").style.width = "0px"; 
+        //document.getElementById("nav-listing").style.lineHeight = "0em";
+
     }
 
 }
